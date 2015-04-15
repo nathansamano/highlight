1d72dea6ae05ec885f8c0a63375c106d822c95ce
Brian Capouch <brianc@saintjoe.edu>
2015-04-15 12:18:35 -0400

    Move highlight script out to its own file

---
+++ js/highlight.js
@@ -0,0 +1,16 @@
+var languages = {
+  javascript: /\b(function|return|var)\b/g /* ... etc */
+};
+
+function highlightAllCode() {
+  var pres = document.body.getElementsByTagName("pre");
+  for (var i = 0; i < pres.length; i++) {
+    var pre = pres[i];
+    var lang = pre.getAttribute("data-language");
+    var name = pre.getAttribute("class-proof");
+console.log('Value of name is ' + name);
+    if (languages.hasOwnProperty(lang))
+      highlightCode(pre, languages[lang]);                                      
+  }                                                                             
+}
+
